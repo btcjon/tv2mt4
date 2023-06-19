480bffecdd565bc8dd61f25f1068fae40f63ec92
Jon Bennett <jonbennett@Jons-MacBook-Pro.local>
2023-06-19 10:52:23 -0400

    Added additional logging and error handling

--- app.py
+++ app.py
@@ -4,30 +4,41 @@ import requests
 import config
 
 app = Flask(__name__)
+app.debug = True
 
 airtable = Airtable(config.AIRTABLE_BASE_ID, config.AIRTABLE_TABLE_NAME, api_key=config.AIRTABLE_API_KEY)
 
 def get_matching_record(symbol):
-    records = airtable.get_all(formula=f"{{Symbol}} = '{symbol}'")
-    return records[0] if records else None
+    try:
+        records = airtable.get_all(formula=f"{{Symbol}} = '{symbol}'")
+        return records[0] if records else None
+    except Exception as e:
+        app.logger.error(f"Error getting record for {symbol}: {e}")
+        return None
 
 def update_airtable_record(record_id, state, last_command):
-    airtable.update(record_id, {'State': state, 'Last Command': last_command})
+    try:
+        airtable.update(record_id, {'State': state, 'Last Command': last_command})
+    except Exception as e:
+        app.logger.error(f"Error updating record {record_id}: {e}")
 
 def send_to_pineconnector(action, symbol, risk):
-    data = f"{config.LICENSE_ID},{action},{symbol},risk={risk}"
-    response = requests.post(config.PINECONNECTOR_WEBHOOK_URL, data=data)
-    print(f"Sent {action} command for {symbol} to Pineconnector, response: {response.text}")
+    try:
+        data = f"{config.LICENSE_ID},{action},{symbol},risk={risk}"
+        response = requests.post(config.PINECONNECTOR_WEBHOOK_URL, data=data)
+        app.logger.info(f"Sent {action} command for {symbol} to Pineconnector, response: {response.text}")
+    except Exception as e:
+        app.logger.error(f"Error sending {action} command for {symbol} to Pineconnector: {e}")
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.data.decode('utf-8')
     command, symbol, *risk = data.split()
     risk = risk[0] if risk else "0.45"
-    print(f"Received {command} command for {symbol} with risk {risk}")
+    app.logger.info(f"Received {command} command for {symbol} with risk {risk}")
     record = get_matching_record(symbol)
     if record:
-        print(f"Found record for {symbol} with state {record['fields']['State']} and trend {record['fields']['Trend']}")
+        app.logger.info(f"Found record for {symbol} with state {record['fields']['State']} and trend {record['fields']['Trend']}")
         if command == "buy" and (record['fields']['State'] != "closed" or record['fields']['Trend'] != "down"):
             send_to_pineconnector(command, symbol, risk)
             update_airtable_record(record['id'], "open", command)
