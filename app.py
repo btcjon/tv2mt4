e92d20ee6af9bc7a42c7c48d755845ece116f9f8
Jon Bennett <jonbennett@Jons-MacBook-Pro.local>
2023-06-20 05:10:14 -0400

    trend1

--- app.py
+++ app.py
@@ -16,8 +16,10 @@ def update_airtable_record(record_id, state, last_command):
 
 def update_airtable_trend(symbol, trend):
     record = get_matching_record(symbol)
+    print(f"Retrieved record: {record}")
     if record:
-        airtable.update(record['id'], {'Trend': trend})
+        response = airtable.update(record['id'], {'Trend': trend})
+        print(f"Airtable update response: {response}")
 
 def send_to_pineconnector(action, symbol, risk):
     data = f"{config.LICENSE_ID},{action},{symbol},risk={risk}"
