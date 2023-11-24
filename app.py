c7e193627c4b12076ee58eb5a57b2b133794eb57
Jon Bennett <jonbennett@Jons-MacBook-Pro.local>
2023-11-24 15:09:59 -0500

    11/24/23-18

--- app.py
+++ app.py
@@ -126,8 +126,14 @@ def webhook():
 def send_pineconnector_command(license_id, command, symbol, risk, tp, sl, comment):
     pineconnector_command = generate_pineconnector_command(license_id, command, symbol, risk, tp, sl, comment)
     app.logger.debug(f"Sending PineConnector command: {pineconnector_command}")
-    response = requests.get(f"{config.PINECONNECTOR_URL}/execute?command={pineconnector_command}")
+    response = requests.get(f"{config.PINECONNECTOR_WEBHOOK_URL}/execute?command={pineconnector_command}")
     app.logger.debug(f"PineConnector response: {response.text}")
+
+    if response.status_code == 200:  # Check the response status code
+        update_airtable_state(symbol, "open", command)
+        update_airtable_count(symbol, command)
+    else:
+        app.logger.error(f"Failed to send PineConnector command: {response.text}")
 
     if response.status_code == 200:  # Check the response status code
         update_airtable_state(symbol, "open", command)
