f3fa6120f792cb6d813d0a9734b4d19146bda9a1
Jon Bennett <jonbennett@Jons-MacBook-Pro.local>
2023-11-26 08:25:01 -0500

    Refactored app.py to consolidate and clean up code, added AirtableOperations class, and updated webhook function to handle different message types.

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-# app.py
 from flask import Flask, request
 import logging
-from airtable_operations import AirtableOperations
+import requests
 import config
 from datetime import datetime, time
+from airtable import Airtable
 
 app = Flask(__name__)
 handler = logging.StreamHandler()
@@ -11,101 +11,117 @@ handler.setLevel(logging.DEBUG)
 app.logger.addHandler(handler)
 app.logger.setLevel(logging.DEBUG)
 
+class AirtableOperations:
+    def __init__(self):
+        self.airtable = Airtable(config.AIRTABLE_BASE_ID, config.AIRTABLE_TABLE_NAME, api_key=config.AIRTABLE_API_KEY)
+        self.logger = logging.getLogger(__name__)
+
+    def get_matching_record(self, symbol):
+        symbol_without_pro = symbol.replace('.PRO', '')
+        records = self.airtable.get_all(formula=f"{{Symbol}} = '{symbol_without_pro}'")
+        return records[0] if records else None
+
+    def update_airtable_field(self, symbol, field, value):
+        record = self.get_matching_record(symbol)
+        self.logger.debug(f"Updating {field} for {symbol} to {value}")
+        if record:
+            response = self.airtable.update(record['id'], {field: value})
+            self.logger.debug(f"Airtable update response: {response}")
+
+    def increment_airtable_field(self, symbol, field):
+        record = self.get_matching_record(symbol)
+        if record:
+            count = record['fields'].get(field, '0')
+            count = str(int(count) + 1)
+            response = self.airtable.update(record['id'], {field: count})
+            self.logger.debug(f"Airtable update response: {response}")
+
+    def reset_airtable_field(self, symbol, field):
+        record = self.get_matching_record(symbol)
+        if record:
+            response = self.airtable.update(record['id'], {field: '0'})
+            self.logger.debug(f"Airtable update response: {response}")
+
 airtable_operations = AirtableOperations()
 
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    try:
+        data = request.data.decode('utf-8')
+        app.logger.debug(f"Received webhook data: {data}")
+        parts = data.split(',')
 
-def generate_pineconnector_command(license_id, command, symbol, risk=None, tp=None, sl=None, comment=None):
-    pineconnector_command = f"{license_id},{command},{symbol}"
+        message_dict = {part.split('=')[0]: part.split('=')[1] for part in parts}
+        message_type = message_dict.get('type')
+        symbol = message_dict.get('symbol')
+
+        if message_type == 'update':
+            keyword = message_dict.get('keyword')
+            if keyword in ['resistance', 'support', 'TD9buy', 'TD9sell']:
+                airtable_operations.update_airtable_field(symbol, keyword.capitalize(), keyword.endswith('OFF'))
+            elif keyword in ['up', 'down']:
+                airtable_operations.update_airtable_field(symbol, 'Trend', keyword)
+        elif message_type == 'order':
+            order_type = message_dict.get('order-type')
+            risk = message_dict.get('risk')
+            tp = message_dict.get('tp')
+            sl = message_dict.get('sl')
+            comment = message_dict.get('comment')
+
+            # Get the current server time
+            now = datetime.utcnow().time()
+
+            # Define the start and end of the restricted period in UTC
+            start = time(21, 55)  # 9:55 PM UTC
+            end = time(23, 0)  # 11:00 PM UTC
+
+            # Check if the current time is within the restricted period
+            if start <= now <= end:
+                return  # If it is, do not send any commands to PineConnector
+
+            if order_type in ['closelong', 'closeshort']:
+                send_pineconnector_command(order_type, symbol, risk, tp, sl, comment)
+            else:
+                record = airtable_operations.get_matching_record(symbol)
+                if record:
+                    bb_present = record['fields'].get('BB')  # get the BB field
+                    if bb_present:
+                        return  # if BB is present, do not send command to PineConnector
+
+                    state_field = 'State Long' if order_type == 'long' else 'State Short'
+                    state = record['fields'].get(state_field)
+                    trend = record['fields'].get('Trend')
+                    snr = record['fields'].get('SnR')
+
+                    if (order_type == "long" and trend == "up" and snr != "Resistance") or (order_type == "short" and trend == "down" and snr != "Support"):
+                        send_pineconnector_command(order_type, symbol, risk, tp, sl, comment)
+
+            if order_type in ['long', 'short']:
+                airtable_operations.update_airtable_field(symbol, f'State {order_type.capitalize()}', 'open')
+                airtable_operations.increment_airtable_field(symbol, f'{order_type.capitalize()}#')
+            elif order_type in ['closelong', 'closeshort']:
+                airtable_operations.update_airtable_field(symbol, f'State {order_type[5:].capitalize()}', 'closed')
+                airtable_operations.reset_airtable_field(symbol, f'{order_type[5:].capitalize()}#')
+
+            return '', 200
+
+    except Exception as e:
+        app.logger.error(f"An error occurred: {e}")
+        return 'Error', 500
+
+def send_pineconnector_command(order_type, symbol, risk, tp, sl, comment):
+    pineconnector_command = f"{config.PINECONNECTOR_LICENSE_ID},{order_type},{symbol}"
     if risk:
-        pineconnector_command += f",{risk}"
+        pineconnector_command += f",risk={risk}"
     if tp:
         pineconnector_command += f",tp={tp}"
     if sl:
         pineconnector_command += f",sl={sl}"
     if comment:
         pineconnector_command += f",comment=\"{comment}\""
-    return pineconnector_command.replace(",tp=None", "").replace(",sl=None", "")
-
-
-# define how to parse the messages
-def handle_zone_found(message):
-    pass  # No action is taken for "Zone Found" scenario
-
-from command_handlers import UpdateSnRHandler, UpdateTrendHandler
-
-# Instantiate command handlers
-update_snr_handler = UpdateSnRHandler(airtable_operations)
-update_trend_handler = UpdateTrendHandler(airtable_operations)
-
-# Update the message_handlers dictionary to use the new command handler instances
-message_handlers = {
-    "enters Support": update_snr_handler.handle,
-    "enters Resistance": update_snr_handler.handle,
-    "is breaking": update_snr_handler.handle,
-    "up": update_trend_handler.handle,
-    "down": update_trend_handler.handle,
-    "flat": update_trend_handler.handle,
-    # ... other handlers will be updated similarly ...
-}
-
-def handle_pineconnector_command(message):
-    parts = message.split(',')
-    if len(parts) < 3:
-        app.logger.error(f"Invalid PineConnector command: {message}")
-        return
-
-    license_id, command, symbol = parts[:3]
-    risk = tp = sl = comment = None
-    for part in parts[3:]:
-        if part.startswith('risk='):
-            risk = part.split('=')[1]
-        elif part.startswith('tp='):
-            tp = part.split('=')[1]
-        elif part.startswith('sl='):
-            sl = part.split('=')[1]
-        elif part.startswith('comment='):
-            comment = part.split('=')[1]
-
-    # Get the current server time
-    now = datetime.utcnow().time()
-
-    # Define the start and end of the restricted period in UTC
-    start = time(21, 55)  # 9:55 PM UTC
-    end = time(23, 0)  # 11:00 PM UTC
-
-    # Check if the current time is within the restricted period
-    if start <= now <= end:
-        return  # If it is, do not send any commands to PineConnector
-
-    if command in ['closelong', 'closeshort']:
-        send_pineconnector_command(license_id, command, symbol, risk, tp, sl, comment)
-    else:
-        record = airtable_operations.get_matching_record(symbol)
-        if record:
-            bb_present = record['fields'].get('BB')  # get the BB field
-            if bb_present:
-                return  # if BB is present, do not send command to PineConnector
-
-            state_field = 'State Long' if command == 'long' else 'State Short'
-            state = record['fields'].get(state_field)
-            trend = record['fields'].get('Trend')
-            snr = record['fields'].get('SnR')
-
-            if (command == "long" and trend == "up" and snr != "Resistance") or (command == "short" and trend == "down" and snr != "Support"):
-                send_pineconnector_command(license_id, command, symbol, risk, tp, sl, comment)
-
-# The duplicated logic for handling PineConnector commands will be consolidated into the handle_pineconnector_command function.
-# The unnecessary code block within the webhook function will be removed.
-
-def send_pineconnector_command(license_id, command, symbol, risk, tp, sl, comment):
-    pineconnector_command = generate_pineconnector_command(license_id, command, symbol, risk, tp, sl, comment)
     app.logger.debug(f"Sending PineConnector command: {pineconnector_command}")
     response = requests.post(config.PINECONNECTOR_WEBHOOK_URL, data=pineconnector_command)
     app.logger.debug(f"PineConnector response: {response.text}")
-
-    if response.status_code == 200:
-        airtable_operations.update_airtable_state(symbol, "In Progress", command)
-        airtable_operations.update_airtable_count(symbol, command)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
