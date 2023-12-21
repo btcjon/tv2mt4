1d022eb7cdad597e566666bf777483662d28be4e
Jon Bennett <jonbennett@Jons-MacBook-Pro.local>
2023-12-21 05:31:27 -0500

    Refactor send_pineconnector_command to handle symbol formatting and include additional parameters.

--- utils.py
+++ utils.py
@@ -1,7 +1,20 @@
+
 import requests
 import config
 import logging
 
 def send_pineconnector_command(order_type, symbol, risk, tp, sl, comment):
-    # ... [Insert the entire logic of send_pineconnector_command from app_old.py here] ...
-    # This includes the entire block of code under the send_pineconnector_command function in app_old.py
+    if not symbol.endswith('.PRO') and symbol != 'USTEC100':
+        symbol += '.PRO'  # append '.PRO' to the symbol only if it's not already there
+    pineconnector_command = f"{config.PINECONNECTOR_LICENSE_ID},{order_type},{symbol}"
+    if risk:
+        pineconnector_command += f",risk={risk}"
+    if tp:
+        pineconnector_command += f",tp={tp}"
+    if sl:
+        pineconnector_command += f",sl={sl}"
+    if comment:
+        # Ensure the comment is included with only a single set of quotes
+        pineconnector_command += f',comment={comment}'
+    response = requests.post(config.PINECONNECTOR_WEBHOOK_URL, data=pineconnector_command)
+    app.logger.debug(f"PineConnector response: {response.status_code} {response.reason} - {response.text}")
